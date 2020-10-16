e3b533fbdd0951e31efcce4a7381a2c91bab2371
Evan Pierre Louis <evan.pierrelouis.github@gmail.com>
2020-10-17 00:10:06 +1100

    Added controllers

--- src/languages/controllers/languages.controller.ts
+++ src/languages/controllers/languages.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
+import { findAllLanguages, findLanguageById } from "data/api/fake-api";
+import { Language } from "data/model/language";
 
 @Controller({path:'languages'})
 export class LanguagesController {
- 
+    @Get()
+    async findAllLanguages(): Promise<Language[]>{
+        return findAllLanguages()
+    }
+
+    @Get(':languageId')
+    async findLanguageById(@Param('languageId') languageId){
+        return findLanguageById(languageId)
+    }
 }
